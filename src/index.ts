99038bcb5b73115e7d7c6003274cfe3243b2353a
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-17 22:34:40 +0900

    class: readonly & static

--- src/index.ts
+++ src/index.ts
@@ -56,21 +56,47 @@ class Dict {
     });
     console.log('삭제완료');
   }
+
+  static hello() {
+    console.log('hello');
+  }
 }
 
 class Word {
-  constructor(public term: string, public def: string) {}
+  //만약 private이나 protected가 아닌 public을 수정불가로 만드려면
+  // TS의 readonly를 추가하면 된다.
+  constructor(public readonly term: string, public readonly def: string) {}
 }
 
 const kimchi = new Word('kimchi', '한국 음식');
 const bibimbob = new Word('bibimbob', '비벼먹는 음식');
 const dict = new Dict();
 
-dict.add(kimchi);
-dict.get('kimchi'); // '한국 음식'
-dict.undate({ term: 'kimchi', def: '매워용' });
-dict.get('kimchi'); // 매워용
-// dict.delete('kimchi');
-// dict.get('kimchi'); // undefined
-dict.add(bibimbob);
-dict.upsert({ term: '김밥', def: '배불러' });
+// kimchi.def = '123123';
+//Cannot assign to 'def' because it is a read-only property.
+
+Dict.hello();
+
+/*
+정적 메서드
+정적 메서드는 클래스의 인스턴스 없이 호출이 가능하며 클래스가 인스턴스화되면 호출할 수 없다. 정적 메서드는 종종 어플리케이션의 유틸리티 함수를 만드는데 사용된다.
+*/
+class Triple {
+  static triple(n?: number) {
+    n = n || 1; //비트연산이 아니어야 합니다.
+    return n * 3;
+  }
+}
+
+class BiggerTriple extends Triple {
+  static triple(n: number) {
+    return super.triple(n) * super.triple(n);
+  }
+}
+
+console.log(Triple.triple()); // 3
+console.log(Triple.triple(6)); // 18
+console.log(BiggerTriple.triple(3)); // 81
+var tp = new Triple();
+console.log(BiggerTriple.triple(3)); // 81 (부모의 인스턴스에 영향을 받지 않습니다.)
+console.log(tp.triple()); // 'tp.triple은 함수가 아닙니다.'.
