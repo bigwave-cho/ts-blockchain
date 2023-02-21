092036735e6f6d9a348a49bf8acbc366b22b34c3
JaehyeonCho93 <hooyaa93@gmail.com>
2023-02-21 11:07:09 +0900

    BlockChain :구현  완료

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,4 @@
-/*
-설정.
-  "scripts": {
-    "build": "tsc", // build파일에 tsc를 이용해서 js파일로 컴파일
-    "dev" : "nodemon --exec ts-node src/index.ts", // 빌드 없이 TS 실행
-    "start": "node build/index.js" // build/index.js를 실행
-  },
-
-  npm i -D ts-node // 빌드 없이 TS를 실행가능(개발 단계에서만 사용)
-  npm i nodemon  //수동으로 서버를 재시작 안해도 반영해줌 (dev 맨 앞에 추가)
-  nodemon 못찾으면 sudo로 설치해보기
-*/
-// import crypto from 'crypto'; //err :Module '"crypto"' has no default export.
-// 해결1: import * as crypto from 'crypto';
-// 해결2: tsconfig  esModuleInterop:true,  allowJs 삭제
 import crypto from 'crypto';
-//crypto declaration err -> @types 찾아서 install
-// or @types/node : nodejs에 대한 모든 것
 
 interface BlockShape {
   hash: string; //블록체인의 고유 서명 같은 것
@@ -25,7 +8,6 @@ interface BlockShape {
 }
 
 class Block implements BlockShape {
-  // hash는 아래 3가지를 이용하여 계산됨
   public hash: string;
   constructor(
     public prevHash: string,
@@ -39,3 +21,64 @@ class Block implements BlockShape {
     return crypto.createHash('sha256').update(toHash).digest('hex');
   }
 }
+
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) {
+      return '';
+    }
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock('first one');
+blockchain.addBlock('second one');
+blockchain.addBlock('third one');
+
+// 원래 getBlocks()이 blocks 원본을 리턴하고 있었기 때문에
+//아래처럼 외부에서 접근하여 데이터 변경이 가능.
+blockchain.getBlocks().push(new Block('xxxx', 1111, 'hackedd'));
+
+//그러나 return [...this.blocks] 로 원본이 아닌 복사본을 리턴하게 되면 접근 불가!
+
+console.log(blockchain.getBlocks());
+
+// [
+//   Block {
+//     prevHash: '',
+//     height: 1,
+//     data: 'first one',
+//     hash: '097283ae04169ff93ba6c732ea4030b841bf7bf1778dae815d0117df765fa825'
+//   },
+//   Block {
+//     prevHash: '097283ae04169ff93ba6c732ea4030b841bf7bf1778dae815d0117df765fa825', //첫 블록과 연결
+//     height: 2,
+//     data: 'second one',
+//     hash: '006340c0d348ae7a93d7c5a7c16daea240d8fa6f66b3c40a8bc6428c029f8d42'
+//   },
+//   Block {
+//     prevHash: '006340c0d348ae7a93d7c5a7c16daea240d8fa6f66b3c40a8bc6428c029f8d42', //두번째 블록과 연결
+//     height: 3,
+//     data: 'third one',
+//     hash: 'e5b0bbe0bd7141b8f262ea74b5859d5eb614f0fe44adf6b3e2d1940a14eb6eaa'
+//   }
+// ]
